ded3fc6e8b97caeb0df93f34917564e92cf17ed5
vasilii-garanin <vasilii_garanin@laptevfamily.com>
2022-02-08 12:43:24 -0600

    WIP

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,21 +1,25 @@
 // implement your posts router here
-const {Router} = require('express');
+const { Router } = require('express');
 const Post = require('./posts-model');
 
-const router = Router()
+const router = Router();
 
-router.get('/', (req, res) => {
-Post.find(req.query)
-.then(posts => {
-    res.status(200).json(posts);
-})
-.catch(error => {
-    console.log(error);
-    res.status(500).json({
-        message: 'Error retrieving the adopters',
-    });
+router.get('/', (req, res) =>
+{
+    Post.find(req.query)
+        .then(posts =>
+        {
+            res.status(200).json(posts);
+        })
+        .catch(error =>
+        {
+            res.status(500).json({
+                message: 'The posts information could not be retrieved',
+                error: error.message,
+                stack: error.stack
+            });
+        });
 });
-})
 
 
 
