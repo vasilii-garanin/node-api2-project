77e7e7b06c514b4d66086684946f492623627f2b
vasilii-garanin <vasilii_garanin@laptevfamily.com>
2022-02-08 12:40:19 -0600

    postRouter get is working

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,25 @@
 // implement your posts router here
+const {Router} = require('express');
+const Post = require('./posts-model');
+
+const router = Router()
+
+router.get('/', (req, res) => {
+Post.find(req.query)
+.then(posts => {
+    res.status(200).json(posts);
+})
+.catch(error => {
+    console.log(error);
+    res.status(500).json({
+        message: 'Error retrieving the adopters',
+    });
+});
+})
+
+
+
+
+
+
+module.exports = router;
